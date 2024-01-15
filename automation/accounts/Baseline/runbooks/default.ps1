4152e0fa8f35b911ff90d49d087cc7669cf6ccbb
Hervé SCLAVON <herve.sclavon@septeo.com>
2024-01-15 13:05:15 -0500

    maj baseline runbook

--- automation/accounts/Baseline/runbooks/default.ps1
+++ automation/accounts/Baseline/runbooks/default.ps1
@@ -1,24 +1,46 @@
-# Ensures you do not inherit an AzContext in your runbook
-Disable-AzContextAutosave -Scope Process
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [object]
+    $WebhookData
+)
 
-# Connect to Azure with system-assigned managed identity
-$AzureContext = (Connect-AzAccount -Identity).context
-
-# Set and store context
-$AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
-
-# Get initial domain name or default domain name
-$Organization = (Get-AzTenant).Domains | Where-Object { $_ -like "*.onmicrosoft.com" }
-
-if ($null -eq $Organization)
+class Body
 {
-    $Organization = (Get-AzTenant).DefaultDomain
+    [string]$Organization
+}
+
+# exit if no provided webhookdata
+if ($null -eq $WebhookData)
+{
+    Write-Error "No webhook data provided."
+    exit
+}
+
+# Logic to allow for testing in Test pane
+if (-Not $WebhookData.RequestBody)
+{ 
+    $WebhookData = $WebhookData | ConvertFrom-Json
+}
+
+try
+{
+    [Body]$requestBody = $WebhookData.RequestBody | ConvertFrom-Json -ErrorAction Stop
+}
+catch
+{
+    Write-Error "Unexpected request body."
+    exit
+}
+finally
+{
+    Write-Output $WebhookData.RequestBody
 }
 
 # Import the ExchangeOnlineManagement Module we imported into the Automation Account
 Import-Module ExchangeOnlineManagement
 
-Connect-ExchangeOnline -ManagedIdentity -Organization $Organization
+Connect-ExchangeOnline -ManagedIdentity -Organization $requestBody.Organization
 
 if ((Get-OrganizationConfig).IsDehydrated)
 {
