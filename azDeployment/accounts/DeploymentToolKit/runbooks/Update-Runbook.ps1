c47a28e151f970a7200613aa4c8ee223db01775a
Hervé SCLAVON <herve.sclavon@septeo.com>
2024-01-29 20:12:10 -0500

    maj

--- azDeployment/accounts/DeploymentToolKit/runbooks/Update-Runbook.ps1
+++ azDeployment/accounts/DeploymentToolKit/runbooks/Update-Runbook.ps1
@@ -11,14 +11,14 @@ workflow Update-RunbookWorkflow
     # Connect to Azure with system-assigned managed identity 
     $null = (Connect-AzAccount -Identity).context    
     
-    $gitObject = $GitHubContent | ConvertFrom-Json
-    $folderPath = $gitObject.Path.Substring(0, $gitObject.Path.lastIndexOf('/'))
+    # $gitObject = $GitHubContent | ConvertFrom-Json
+    # $folderPath = $gitObject.Path.Substring(0, $gitObject.Path.lastIndexOf('/'))
 
-    $subscriptions = Get-AzSubscription | Where-Object { $_.ExtendedProperties.ManagedByTenants }
+    # $subscriptions = Get-AzSubscription | Where-Object { $_.ExtendedProperties.ManagedByTenants }
 
-    foreach -Parallel ($subscription in $subscriptions)
-    {
-        $azContext = Set-AzContext -Subscription $subscription.Id -ErrorAction Stop
-        Get-AzAutomationAccount | Get-AzAutomationSourceControl | Where-Object { $_.FolderPath -match $folderPath }
-    }    
+    # foreach -Parallel ($subscription in $subscriptions)
+    # {
+    #     $azContext = Set-AzContext -Subscription $subscription.Id -ErrorAction Stop
+    #     Get-AzAutomationAccount | Get-AzAutomationSourceControl | Where-Object { $_.FolderPath -match $folderPath }
+    # }    
 }
