e6cdb9957c4ae6218f9c205481cf4c3ddcf0c9a3
Hervé SCLAVON <herve.sclavon@septeo.com>
2024-01-29 23:47:20 -0500

    maj

--- azDeployment/accounts/DeploymentToolKit/runbooks/Update-Runbook.ps1
+++ azDeployment/accounts/DeploymentToolKit/runbooks/Update-Runbook.ps1
@@ -6,14 +6,19 @@ workflow Update-Runbook
         [object]$GitHubContent
     )
     
+    $gitObject = $GitHubContent | ConvertFrom-Json
+
+    Write-Output $gitObject
+
+    $folderPath = $gitObject.Path.Substring(0, $gitObject.Path.lastIndexOf('/'))
+
+    Write-Output $folderPath
+
     # Ensures you do not inherit an AzContext in your runbook
     $null = Disable-AzContextAutosave -Scope Process
     
     # Connect to Azure with system-assigned managed identity 
     $null = (Connect-AzAccount -Identity).context    
-    
-    $gitObject = $GitHubContent | ConvertFrom-Json
-    $folderPath = $gitObject.Path.Substring(0, $gitObject.Path.lastIndexOf('/'))
 
     $subscriptions = Get-AzSubscription | Where-Object { $_.ExtendedProperties.ManagedByTenants }
 
