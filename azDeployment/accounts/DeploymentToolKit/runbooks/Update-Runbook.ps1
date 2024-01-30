3dee8a58c4b3d93aaec183a19b9b598588a23a90
Hervé SCLAVON <herve.sclavon@septeo.com>
2024-01-29 20:15:34 -0500

    maj

--- azDeployment/accounts/DeploymentToolKit/runbooks/Update-Runbook.ps1
+++ azDeployment/accounts/DeploymentToolKit/runbooks/Update-Runbook.ps1
@@ -1,21 +1,24 @@
-param (
-    [Parameter(Mandatory)]
-    [object]$GitHubContent
-)
-    
-# Ensures you do not inherit an AzContext in your runbook
-$null = Disable-AzContextAutosave -Scope Process
-    
-# Connect to Azure with system-assigned managed identity 
-$null = (Connect-AzAccount -Identity).context    
-    
-$gitObject = $GitHubContent | ConvertFrom-Json
-$folderPath = $gitObject.Path.Substring(0, $gitObject.Path.lastIndexOf('/'))
-
-$subscriptions = Get-AzSubscription | Where-Object { $_.ExtendedProperties.ManagedByTenants }
-
-foreach ($subscription in $subscriptions)
+workflow Update-RunbookWorkflow
 {
-    $azContext = Set-AzContext -Subscription $subscription.Id -ErrorAction Stop
-    Get-AzAutomationAccount | Get-AzAutomationSourceControl | Where-Object { $_.FolderPath -match $folderPath }
-}    
+    param (
+        [Parameter(Mandatory)]
+        [string]$GitHubContent
+    )
+    
+    # Ensures you do not inherit an AzContext in your runbook
+    $null = Disable-AzContextAutosave -Scope Process
+    
+    # Connect to Azure with system-assigned managed identity 
+    $null = (Connect-AzAccount -Identity).context    
+    
+    $gitObject = $GitHubContent | ConvertFrom-Json
+    $folderPath = $gitObject.Path.Substring(0, $gitObject.Path.lastIndexOf('/'))
+
+    $subscriptions = Get-AzSubscription | Where-Object { $_.ExtendedProperties.ManagedByTenants }
+
+    foreach -Parallel ($subscription in $subscriptions)
+    {
+        $azContext = Set-AzContext -Subscription $subscription.Id -ErrorAction Stop
+        Get-AzAutomationAccount | Get-AzAutomationSourceControl | Where-Object { $_.FolderPath -match $folderPath }
+    }    
+}
