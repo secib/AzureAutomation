d2fefaf112918ee79d66757f792eb4efaed39ea1
Hervé SCLAVON <herve.sclavon@septeo.com>
2024-01-30 13:02:49 -0500

    maj function

--- azDeployment/accounts/DeploymentToolKit/runbooks/Update-Runbook.ps1
+++ azDeployment/accounts/DeploymentToolKit/runbooks/Update-Runbook.ps1
@@ -1,28 +1,26 @@
-workflow Update-Runbook
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)]
+    [object]$GitHubContent
+)
+    
+Write-Output $GitHubContent
+
+$folderPath = $GitHubContent.Path.Substring(0, $GitHubContent.Path.lastIndexOf('/'))
+
+Write-Output $folderPath
+
+# Ensures you do not inherit an AzContext in your runbook
+$null = Disable-AzContextAutosave -Scope Process
+    
+# Connect to Azure with system-assigned managed identity 
+$null = (Connect-AzAccount -Identity).context    
+
+$subscriptions = , (Get-AzSubscription | Where-Object { $_.ExtendedProperties.ManagedByTenants })
+
+foreach ($subscription in $subscriptions)
 {
-    param (
-        [Parameter(Mandatory)]
-        [object]$GitHubContent
-    )
-    
-    Write-Output $GitHubContent
-
-    $folderPath = $GitHubContent.Path.Substring(0, $GitHubContent.Path.lastIndexOf('/'))
-
-    Write-Output $folderPath
-
-    # Ensures you do not inherit an AzContext in your runbook
-    $null = Disable-AzContextAutosave -Scope Process
-    
-    # Connect to Azure with system-assigned managed identity 
-    $null = (Connect-AzAccount -Identity).context    
-
-    [array]$subscriptions = Get-AzSubscription | Where-Object { $_.ExtendedProperties.ManagedByTenants }
-
-    foreach -Parallel ($subscription in $subscriptions)
-    {
-        Write-Output $subscription.Id
-        $azContext = Set-AzContext -Subscription $subscription.Id -ErrorAction Stop
-        Get-AzAutomationAccount | Get-AzAutomationSourceControl | Where-Object { $_.FolderPath -match $folderPath } | Start-AzAutomationSourceControlSyncJob
-    }    
-}
+    Write-Output $subscription.Id
+    $azContext = Set-AzContext -Subscription $subscription.Id -ErrorAction Stop
+    Get-AzAutomationAccount | Get-AzAutomationSourceControl | Where-Object { $_.FolderPath -match $folderPath } | Start-AzAutomationSourceControlSyncJob
+}    
