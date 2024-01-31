1b000b9f46b2803bdac72018460b2804d22b407c
Hervé SCLAVON <herve.sclavon@septeo.com>
2024-01-31 11:02:05 -0500

    nettoyage

--- test/Test-Webhook copy.ps1
+++ test/Test-Webhook copy.ps1
@@ -1,115 +1,5 @@
-$automationAccount = "AdministrativeTask"
-$resourceGroup = "Automation"
-$webhookURI = "https://0101fce2-c71c-45c7-8b8f-59e626f6f35a.webhook.fc.azure-automation.net/webhooks?token=cofFVZipvIol4OHrF9y%2f7klCkZaQgX9camMZ7wkM3ho%3d"
-$body = @"
-{
-    "subscriptionId": "8401406c-b573-4533-8d77-df8803de5153",
-    "notificationId": 1,
-    "id": "1e1dad80-a40f-4c6e-ae8a-e1e41b390216",
-    "eventType": "git.push",
-    "publisherId": "tfs",
-    "message": {
-      "text": "Hervé SCLAVON pushed updates to AzDeploymentToolKitTest:main\r\n(https://dev.azure.com/AzDeploymentToolkitTestProject/AzDeploymentToolKitTest/_git/AzDeploymentToolKitTest/#version=GBmain)",
-      "html": "Herv&#233; SCLAVON pushed updates to <a href=\"https://dev.azure.com/AzDeploymentToolkitTestProject/AzDeploymentToolKitTest/_git/AzDeploymentToolKitTest/\">AzDeploymentToolKitTest</a>:<a href=\"https://dev.azure.com/AzDeploymentToolkitTestProject/AzDeploymentToolKitTest/_git/AzDeploymentToolKitTest/#version=GBmain\">main</a>",
-      "markdown": "Hervé SCLAVON pushed updates to [AzDeploymentToolKitTest](https://dev.azure.com/AzDeploymentToolkitTestProject/AzDeploymentToolKitTest/_git/AzDeploymentToolKitTest/):[main](https://dev.azure.com/AzDeploymentToolkitTestProject/AzDeploymentToolKitTest/_git/AzDeploymentToolKitTest/#version=GBmain)"
-    },
-    "detailedMessage": {
-      "text": "Hervé SCLAVON pushed a commit to AzDeploymentToolKitTest:main\r\n - ajout commentaire 9f961760 (https://dev.azure.com/AzDeploymentToolkitTestProject/AzDeploymentToolKitTest/_git/AzDeploymentToolKitTest/commit/9f9617607c680eae5395412f9dcbf5dc185c3913)",
-      "html": "Herv&#233; SCLAVON pushed a commit to <a href=\"https://dev.azure.com/AzDeploymentToolkitTestProject/AzDeploymentToolKitTest/_git/AzDeploymentToolKitTest/\">AzDeploymentToolKitTest</a>:<a href=\"https://dev.azure.com/AzDeploymentToolkitTestProject/AzDeploymentToolKitTest/_git/AzDeploymentToolKitTest/#version=GBmain\">main</a>\r\n<ul>\r\n<li>ajout commentaire <a href=\"https://dev.azure.com/AzDeploymentToolkitTestProject/AzDeploymentToolKitTest/_git/AzDeploymentToolKitTest/commit/9f9617607c680eae5395412f9dcbf5dc185c3913\">9f961760</a></li>\r\n</ul>",
-      "markdown": "Hervé SCLAVON pushed a commit to [AzDeploymentToolKitTest](https://dev.azure.com/AzDeploymentToolkitTestProject/AzDeploymentToolKitTest/_git/AzDeploymentToolKitTest/):[main](https://dev.azure.com/AzDeploymentToolkitTestProject/AzDeploymentToolKitTest/_git/AzDeploymentToolKitTest/#version=GBmain)\r\n* ajout commentaire [9f961760](https://dev.azure.com/AzDeploymentToolkitTestProject/AzDeploymentToolKitTest/_git/AzDeploymentToolKitTest/commit/9f9617607c680eae5395412f9dcbf5dc185c3913)"
-    },
-    "resource": {
-      "commits": [
-        {
-          "commitId": "9f9617607c680eae5395412f9dcbf5dc185c3913",
-          "author": {
-            "name": "Hervé SCLAVON",
-            "email": "herve.sclavon@septeo.com",
-            "date": "2024-01-18T20:26:39Z"
-          },
-          "committer": {
-            "name": "Hervé SCLAVON",
-            "email": "herve.sclavon@septeo.com",
-            "date": "2024-01-18T20:26:39Z"
-          },
-          "comment": "ajout commentaire",
-          "url": "https://dev.azure.com/AzDeploymentToolkitTestProject/_apis/git/repositories/2fc13ae2-c67a-4aac-b9c2-f9db228d1eb5/commits/9f9617607c680eae5395412f9dcbf5dc185c3913"
-        }
-      ],
-      "refUpdates": [
-        {
-          "name": "refs/heads/main",
-          "oldObjectId": "143354fc8f1329f364400464f08046c3e74a467a",
-          "newObjectId": "9f9617607c680eae5395412f9dcbf5dc185c3913"
-        }
-      ],
-      "repository": {
-        "id": "2fc13ae2-c67a-4aac-b9c2-f9db228d1eb5",
-        "name": "AzDeploymentToolKitTest",
-        "url": "https://dev.azure.com/AzDeploymentToolkitTestProject/_apis/git/repositories/2fc13ae2-c67a-4aac-b9c2-f9db228d1eb5",
-        "project": {
-          "id": "fe9774a6-af7b-43d4-a0e6-f85cf0139f3b",
-          "name": "AzDeploymentToolKitTest",
-          "url": "https://dev.azure.com/AzDeploymentToolkitTestProject/_apis/projects/fe9774a6-af7b-43d4-a0e6-f85cf0139f3b",
-          "state": "wellFormed",
-          "visibility": "unchanged",
-          "lastUpdateTime": "0001-01-01T00:00:00"
-        },
-        "defaultBranch": "refs/heads/main",
-        "remoteUrl": "https://dev.azure.com/AzDeploymentToolkitTestProject/AzDeploymentToolKitTest/_git/AzDeploymentToolKitTest"
-      },
-      "pushedBy": {
-        "displayName": "Hervé SCLAVON",
-        "url": "https://spsprodneu1.vssps.visualstudio.com/Ad96ba4f3-f9b2-4375-b39c-e2749c12e5d5/_apis/Identities/f19ce07c-8497-6098-b23e-89142ea65e0b",
-        "_links": {
-          "avatar": {
-            "href": "https://dev.azure.com/AzDeploymentToolkitTestProject/_apis/GraphProfile/MemberAvatars/aad.ZjE5Y2UwN2MtODQ5Ny03MDk4LWIyM2UtODkxNDJlYTY1ZTBi"
-          }
-        },
-        "id": "f19ce07c-8497-6098-b23e-89142ea65e0b",
-        "uniqueName": "herve.sclavon@septeo.com",
-        "imageUrl": "https://dev.azure.com/AzDeploymentToolkitTestProject/_api/_common/identityImage?id=f19ce07c-8497-6098-b23e-89142ea65e0b",
-        "descriptor": "aad.ZjE5Y2UwN2MtODQ5Ny03MDk4LWIyM2UtODkxNDJlYTY1ZTBi"
-      },
-      "pushId": 6,
-      "date": "2024-01-18T20:26:56.6363943Z",
-      "url": "https://dev.azure.com/AzDeploymentToolkitTestProject/_apis/git/repositories/2fc13ae2-c67a-4aac-b9c2-f9db228d1eb5/pushes/6",
-      "_links": {
-        "self": {
-          "href": "https://dev.azure.com/AzDeploymentToolkitTestProject/_apis/git/repositories/2fc13ae2-c67a-4aac-b9c2-f9db228d1eb5/pushes/6"
-        },
-        "repository": {
-          "href": "https://dev.azure.com/AzDeploymentToolkitTestProject/fe9774a6-af7b-43d4-a0e6-f85cf0139f3b/_apis/git/repositories/2fc13ae2-c67a-4aac-b9c2-f9db228d1eb5"
-        },
-        "commits": {
-          "href": "https://dev.azure.com/AzDeploymentToolkitTestProject/_apis/git/repositories/2fc13ae2-c67a-4aac-b9c2-f9db228d1eb5/pushes/6/commits"
-        },
-        "pusher": {
-          "href": "https://spsprodneu1.vssps.visualstudio.com/Ad96ba4f3-f9b2-4375-b39c-e2749c12e5d5/_apis/Identities/f19ce07c-8497-6098-b23e-89142ea65e0b"
-        },
-        "refs": {
-          "href": "https://dev.azure.com/AzDeploymentToolkitTestProject/fe9774a6-af7b-43d4-a0e6-f85cf0139f3b/_apis/git/repositories/2fc13ae2-c67a-4aac-b9c2-f9db228d1eb5/refs/heads/main"
-        }
-      }
-    },
-    "resourceVersion": "1.0",
-    "resourceContainers": {
-      "collection": {
-        "id": "e943cfc2-0c28-4e54-80b5-4d4f1f3f21cc",
-        "baseUrl": "https://dev.azure.com/AzDeploymentToolkitTestProject/"
-      },
-      "account": {
-        "id": "d96ba4f3-f9b2-4375-b39c-e2749c12e5d5",
-        "baseUrl": "https://dev.azure.com/AzDeploymentToolkitTestProject/"
-      },
-      "project": {
-        "id": "fe9774a6-af7b-43d4-a0e6-f85cf0139f3b",
-        "baseUrl": "https://dev.azure.com/AzDeploymentToolkitTestProject/"
-      }
-    },
-    "createdDate": "2024-01-18T20:27:03.5095237Z"
-  }  
-"@
+$webhookURI = "https://cb632b6a-8bb5-443c-a57e-436fc74422e1.webhook.fc.azure-automation.net/webhooks?token=JR1oJRuyiTPMB2nyVFfTTr7WGylRXUV31zBkV8Ibw4w%3d"
+$body = '{"AutomationAccountDeploymentBuilders":[{"SubscriptionId":"ffb58682-139a-41b1-b4b3-20fa17ed741c","ResourceGroupDeployment":{"ResourceGroupName":"Automation","TemplateUri":"https://raw.githubusercontent.com/secib/AzureAutomation/main/automation/accounts/AdministrativeTask/templates/armTemplate_administrativeTask.json","TemplateParameterUri":"https://raw.githubusercontent.com/secib/AzureAutomation/main/automation/accounts/AdministrativeTask/templates/armTemplate_administrativeTask.parameters.json"},"ApplicationRoleAssignments":[{"ResourceAppId":"00000003-0000-0000-c000-000000000000","ApplicationRoles":["User.ReadWrite.All"]}],"DirectoryRoles":[]},{"SubscriptionId":"ffb58682-139a-41b1-b4b3-20fa17ed741c","ResourceGroupDeployment":{"ResourceGroupName":"Automation","TemplateUri":"https://raw.githubusercontent.com/secib/AzureAutomation/main/automation/accounts/Baseline/templates/baseline.json","TemplateParameterUri":"https://raw.githubusercontent.com/secib/AzureAutomation/main/automation/accounts/Baseline/templates/baseline.parameters.json"},"ApplicationRoleAssignments":[{"ResourceAppId":"00000002-0000-0ff1-ce00-000000000000","ApplicationRoles":["Exchange.ManageAsApp"]}],"DirectoryRoles":["Exchange Administrator"]}]}'
 
 $responseFile = Invoke-WebRequest -Method Post -Uri $webhookURI -Body $body -UseBasicParsing
 $responseFile
@@ -119,12 +9,12 @@ $jobid = (ConvertFrom-Json ($responseFile.Content)).jobids[0]
 
 # Get output
 Get-AzAutomationJobOutput `
-    -AutomationAccountName $automationAccount `
-    -Id $jobid `
-    -ResourceGroupName $resourceGroup `
-    -Stream Output
+  -AutomationAccountName $automationAccount `
+  -Id $jobid `
+  -ResourceGroupName $resourceGroup `
+  -Stream Output
 
 Get-AzAutomationJob `
-    -AutomationAccountName $automationAccount `
-    -Id $jobid `
-    -ResourceGroupName $resourceGroup `
+  -AutomationAccountName $automationAccount `
+  -Id $jobid `
+  -ResourceGroupName $resourceGroup `
---
+++ test/test.ps1
@@ -0,0 +1,210 @@
+$payload = @"
+{
+    "ref": "refs/heads/main",
+    "before": "d2fefaf112918ee79d66757f792eb4efaed39ea1",
+    "after": "4f9292ca3999e5889ef00ca76ff292bf0556aa2a",
+    "repository": {
+      "id": 736409190,
+      "node_id": "R_kgDOK-S2Zg",
+      "name": "AzureAutomation",
+      "full_name": "secib/AzureAutomation",
+      "private": false,
+      "owner": {
+        "name": "secib",
+        "email": "55201003+secib@users.noreply.github.com",
+        "login": "secib",
+        "id": 55201003,
+        "node_id": "MDQ6VXNlcjU1MjAxMDAz",
+        "avatar_url": "https://avatars.githubusercontent.com/u/55201003?v=4",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/secib",
+        "html_url": "https://github.com/secib",
+        "followers_url": "https://api.github.com/users/secib/followers",
+        "following_url": "https://api.github.com/users/secib/following{/other_user}",
+        "gists_url": "https://api.github.com/users/secib/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/secib/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/secib/subscriptions",
+        "organizations_url": "https://api.github.com/users/secib/orgs",
+        "repos_url": "https://api.github.com/users/secib/repos",
+        "events_url": "https://api.github.com/users/secib/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/secib/received_events",
+        "type": "User",
+        "site_admin": false
+      },
+      "html_url": "https://github.com/secib/AzureAutomation",
+      "description": null,
+      "fork": false,
+      "url": "https://github.com/secib/AzureAutomation",
+      "forks_url": "https://api.github.com/repos/secib/AzureAutomation/forks",
+      "keys_url": "https://api.github.com/repos/secib/AzureAutomation/keys{/key_id}",
+      "collaborators_url": "https://api.github.com/repos/secib/AzureAutomation/collaborators{/collaborator}",
+      "teams_url": "https://api.github.com/repos/secib/AzureAutomation/teams",
+      "hooks_url": "https://api.github.com/repos/secib/AzureAutomation/hooks",
+      "issue_events_url": "https://api.github.com/repos/secib/AzureAutomation/issues/events{/number}",
+      "events_url": "https://api.github.com/repos/secib/AzureAutomation/events",
+      "assignees_url": "https://api.github.com/repos/secib/AzureAutomation/assignees{/user}",
+      "branches_url": "https://api.github.com/repos/secib/AzureAutomation/branches{/branch}",
+      "tags_url": "https://api.github.com/repos/secib/AzureAutomation/tags",
+      "blobs_url": "https://api.github.com/repos/secib/AzureAutomation/git/blobs{/sha}",
+      "git_tags_url": "https://api.github.com/repos/secib/AzureAutomation/git/tags{/sha}",
+      "git_refs_url": "https://api.github.com/repos/secib/AzureAutomation/git/refs{/sha}",
+      "trees_url": "https://api.github.com/repos/secib/AzureAutomation/git/trees{/sha}",
+      "statuses_url": "https://api.github.com/repos/secib/AzureAutomation/statuses/{sha}",
+      "languages_url": "https://api.github.com/repos/secib/AzureAutomation/languages",
+      "stargazers_url": "https://api.github.com/repos/secib/AzureAutomation/stargazers",
+      "contributors_url": "https://api.github.com/repos/secib/AzureAutomation/contributors",
+      "subscribers_url": "https://api.github.com/repos/secib/AzureAutomation/subscribers",
+      "subscription_url": "https://api.github.com/repos/secib/AzureAutomation/subscription",
+      "commits_url": "https://api.github.com/repos/secib/AzureAutomation/commits{/sha}",
+      "git_commits_url": "https://api.github.com/repos/secib/AzureAutomation/git/commits{/sha}",
+      "comments_url": "https://api.github.com/repos/secib/AzureAutomation/comments{/number}",
+      "issue_comment_url": "https://api.github.com/repos/secib/AzureAutomation/issues/comments{/number}",
+      "contents_url": "https://api.github.com/repos/secib/AzureAutomation/contents/{+path}",
+      "compare_url": "https://api.github.com/repos/secib/AzureAutomation/compare/{base}...{head}",
+      "merges_url": "https://api.github.com/repos/secib/AzureAutomation/merges",
+      "archive_url": "https://api.github.com/repos/secib/AzureAutomation/{archive_format}{/ref}",
+      "downloads_url": "https://api.github.com/repos/secib/AzureAutomation/downloads",
+      "issues_url": "https://api.github.com/repos/secib/AzureAutomation/issues{/number}",
+      "pulls_url": "https://api.github.com/repos/secib/AzureAutomation/pulls{/number}",
+      "milestones_url": "https://api.github.com/repos/secib/AzureAutomation/milestones{/number}",
+      "notifications_url": "https://api.github.com/repos/secib/AzureAutomation/notifications{?since,all,participating}",
+      "labels_url": "https://api.github.com/repos/secib/AzureAutomation/labels{/name}",
+      "releases_url": "https://api.github.com/repos/secib/AzureAutomation/releases{/id}",
+      "deployments_url": "https://api.github.com/repos/secib/AzureAutomation/deployments",
+      "created_at": 1703709239,
+      "updated_at": "2023-12-27T21:00:15Z",
+      "pushed_at": 1706638112,
+      "git_url": "git://github.com/secib/AzureAutomation.git",
+      "ssh_url": "git@github.com:secib/AzureAutomation.git",
+      "clone_url": "https://github.com/secib/AzureAutomation.git",
+      "svn_url": "https://github.com/secib/AzureAutomation",
+      "homepage": null,
+      "size": 83,
+      "stargazers_count": 0,
+      "watchers_count": 0,
+      "language": "PowerShell",
+      "has_issues": true,
+      "has_projects": true,
+      "has_downloads": true,
+      "has_wiki": true,
+      "has_pages": false,
+      "has_discussions": false,
+      "forks_count": 0,
+      "mirror_url": null,
+      "archived": false,
+      "disabled": false,
+      "open_issues_count": 0,
+      "license": null,
+      "allow_forking": true,
+      "is_template": false,
+      "web_commit_signoff_required": false,
+      "topics": [
+  
+      ],
+      "visibility": "public",
+      "forks": 0,
+      "open_issues": 0,
+      "watchers": 0,
+      "default_branch": "main",
+      "stargazers": 0,
+      "master_branch": "main"
+    },
+    "pusher": {
+      "name": "cassfras21",
+      "email": "github@cassfras.fr"
+    },
+    "sender": {
+      "login": "cassfras21",
+      "id": 20661140,
+      "node_id": "MDQ6VXNlcjIwNjYxMTQw",
+      "avatar_url": "https://avatars.githubusercontent.com/u/20661140?v=4",
+      "gravatar_id": "",
+      "url": "https://api.github.com/users/cassfras21",
+      "html_url": "https://github.com/cassfras21",
+      "followers_url": "https://api.github.com/users/cassfras21/followers",
+      "following_url": "https://api.github.com/users/cassfras21/following{/other_user}",
+      "gists_url": "https://api.github.com/users/cassfras21/gists{/gist_id}",
+      "starred_url": "https://api.github.com/users/cassfras21/starred{/owner}{/repo}",
+      "subscriptions_url": "https://api.github.com/users/cassfras21/subscriptions",
+      "organizations_url": "https://api.github.com/users/cassfras21/orgs",
+      "repos_url": "https://api.github.com/users/cassfras21/repos",
+      "events_url": "https://api.github.com/users/cassfras21/events{/privacy}",
+      "received_events_url": "https://api.github.com/users/cassfras21/received_events",
+      "type": "User",
+      "site_admin": false
+    },
+    "created": false,
+    "deleted": false,
+    "forced": false,
+    "base_ref": null,
+    "compare": "https://github.com/secib/AzureAutomation/compare/d2fefaf11291...4f9292ca3999",
+    "commits": [
+      {
+        "id": "4f9292ca3999e5889ef00ca76ff292bf0556aa2a",
+        "tree_id": "7b9c4ac10460d09f3eb4aece74f2ed8f6b84f33e",
+        "distinct": true,
+        "message": "test",
+        "timestamp": "2024-01-30T13:05:37-05:00",
+        "url": "https://github.com/secib/AzureAutomation/commit/4f9292ca3999e5889ef00ca76ff292bf0556aa2a",
+        "author": {
+          "name": "Hervé SCLAVON",
+          "email": "herve.sclavon@septeo.com"
+        },
+        "committer": {
+          "name": "Hervé SCLAVON",
+          "email": "herve.sclavon@septeo.com"
+        },
+        "added": [
+  
+        ],
+        "removed": [
+  
+        ],
+        "modified": [
+          "automation/accounts/Baseline/runbooks/default.ps1"
+        ]
+      }
+    ],
+    "head_commit": {
+      "id": "4f9292ca3999e5889ef00ca76ff292bf0556aa2a",
+      "tree_id": "7b9c4ac10460d09f3eb4aece74f2ed8f6b84f33e",
+      "distinct": true,
+      "message": "test",
+      "timestamp": "2024-01-30T13:05:37-05:00",
+      "url": "https://github.com/secib/AzureAutomation/commit/4f9292ca3999e5889ef00ca76ff292bf0556aa2a",
+      "author": {
+        "name": "Hervé SCLAVON",
+        "email": "herve.sclavon@septeo.com"
+      },
+      "committer": {
+        "name": "Hervé SCLAVON",
+        "email": "herve.sclavon@septeo.com"
+      },
+      "added": [
+  
+      ],
+      "removed": [
+  
+      ],
+      "modified": [
+        "automation/accounts/Baseline/runbooks/default.ps1"
+      ]
+    }
+  }
+"@
+
+function Get-ModifiedFolderPathFromGitPushEvent
+{
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]
+        $Body
+    )
+
+    $object = $Body | ConvertFrom-Json
+    return , ($object.commits.modified | ForEach-Object { $_.Substring(0, $_.lastIndexOf('/')) } | Sort-Object -Unique)
+}
+
+$folderPath = Get-ModifiedFolderPathFromGitPushEvent -Body $payload
+$folderPath.GetType()
